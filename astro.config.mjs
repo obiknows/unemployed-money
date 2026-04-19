1016e7575c00d965c3a26105609e3d3a7004c669
Obinna <10480589+obiknows@users.noreply.github.com>
2026-04-19 00:44:25 -0400

    chore: ignore vercel build output

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
+import vercel from '@astrojs/vercel';
 
-// https://astro.build/config
 export default defineConfig({
+  output: 'static',
+  adapter: vercel(),
   integrations: [react(), markdoc(), keystatic()],
 });
